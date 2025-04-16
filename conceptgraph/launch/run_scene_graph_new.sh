631ed6c939c0bb828254caa6f9b64c8b803de0be
Justin Lidard <jlidard@princeton.edu>
2025-04-16 17:13:48 -0400

    minor

--- conceptgraph/launch/run_scene_graph_new.sh
+++ conceptgraph/launch/run_scene_graph_new.sh
@@ -6,11 +6,11 @@ CONFIG_PATH=/home/jlidard/concept-graphs/conceptgraph/dataset/dataconfigs/zed2i/
 CLASS_SET=ram
 PKL_FILENAME=full_pcd_ram_withbg_allclasses_OUTSIDE_post.pkl.gz # Change this to the actual output file name of the pkl.gz file
 
-# python scenegraph/build_scenegraph_cfslam_gpt.py \
-#     --mode preprocess_images \
-#     --masking_option red_outline \
-#     --cachedir ${ROOT_DIR}/${SCENE_NAME}/sg_cache \
-#     --mapfile ${ROOT_DIR}/${SCENE_NAME}/pcd_saves/${PKL_FILENAME}
+python scenegraph/build_scenegraph_cfslam_gpt.py \
+    --mode preprocess_images \
+    --masking_option red_outline \
+    --cachedir ${ROOT_DIR}/${SCENE_NAME}/sg_cache \
+    --mapfile ${ROOT_DIR}/${SCENE_NAME}/pcd_saves/${PKL_FILENAME}
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate pbrick
